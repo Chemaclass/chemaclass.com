19b8fd609196ae441d78f7f134158496b6e7bd2a
Chemaclass <chemaclass@outlook.es>
2024-06-02 11:55:26 +0200

    use light theme by default

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,33 +1,35 @@
-const THEME_FOR_FIRST_TIME_USERS = 'dark';
+const THEME_KEY = 'theme';
+const THEME_LIGHT = 'light';
+const THEME_DARK = 'dark';
 
-if (!localStorage.getItem('theme')) {
-  localStorage.setItem('theme', THEME_FOR_FIRST_TIME_USERS);
+if (!localStorage.getItem(THEME_KEY)) {
+  localStorage.setItem(THEME_KEY, THEME_LIGHT);
 }
 
-const currentTheme = localStorage.getItem('theme');
+const currentTheme = localStorage.getItem(THEME_KEY);
 const toggleDiv = document.getElementById('theme-toggle');
 const bodyClass = document.body.classList;
 
-if (currentTheme === 'dark') {
-  bodyClass.add('dark');
-  bodyClass.remove('light');
+if (currentTheme === THEME_DARK) {
+  bodyClass.add(THEME_DARK);
+  bodyClass.remove(THEME_LIGHT);
   toggleDiv.innerHTML = "🌑";
 } else {
-  bodyClass.add('light');
-  bodyClass.remove('dark');
+  bodyClass.add(THEME_LIGHT);
+  bodyClass.remove(THEME_DARK);
   toggleDiv.innerHTML = "☀️";
 }
 
 function toggleTheme() {
-  if (bodyClass.contains('dark')) {
-    localStorage.setItem('theme', 'light');
-    bodyClass.add('light');
-    bodyClass.remove('dark');
+  if (bodyClass.contains(THEME_DARK)) {
+    localStorage.setItem(THEME_KEY, THEME_LIGHT);
+    bodyClass.add(THEME_LIGHT);
+    bodyClass.remove(THEME_DARK);
     toggleDiv.innerHTML = "☀️";
   } else {
-    localStorage.setItem('theme', 'dark');
-    bodyClass.add('dark');
-    bodyClass.remove('light');
+    localStorage.setItem(THEME_KEY, THEME_DARK);
+    bodyClass.add(THEME_DARK);
+    bodyClass.remove(THEME_LIGHT);
     toggleDiv.innerHTML = "🌑";
   }
 }
