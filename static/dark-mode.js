46c581b190a8c2ff2d7ba2cc417c26ccd02d3ad9
Chemaclass <chemaclass@outlook.es>
2025-11-27 13:48:51 +0100

    Revert "fix homopage navbar menu for small screens"
    This reverts commit 702807053de37b6842f4a58442e4299259bf504a.

--- static/dark-mode.js
+++ static/dark-mode.js
@@ -10,13 +10,6 @@ window.toLightMode = function () {
   window.updateTheme();
 }
 
-window.toggleHomepageMenu = function () {
-  const nav = document.getElementById("homepage-nav");
-  if (nav) {
-    nav.classList.toggle("homepage-nav-open");
-  }
-}
-
 function updateTheme() {
   const snowOn = document.getElementById("snow-on");
   const snowOff = document.getElementById("snow-off");
@@ -27,24 +20,16 @@ function updateTheme() {
       document.documentElement.classList.add('theme-dark');
       document.documentElement.setAttribute('color-theme', 'dark');
 
-      if (snowOn && snowOn.querySelector("svg")) {
-        snowOn.querySelector("svg").setAttribute("fill", "deepskyblue");
-      }
-      if (snowOff && snowOff.querySelector("svg")) {
-        snowOff.querySelector("svg").setAttribute("fill", "#c5c5c5");
-      }
+      snowOn.querySelector("svg").setAttribute("fill", "deepskyblue");
+      snowOff.querySelector("svg").setAttribute("fill", "#c5c5c5");
       break;
 
     default:
       document.documentElement.classList.remove('theme-dark');
       document.documentElement.setAttribute('color-theme', 'light');
 
-      if (snowOn && snowOn.querySelector("svg")) {
-        snowOn.querySelector("svg").setAttribute("fill", "#444");
-      }
-      if (snowOff && snowOff.querySelector("svg")) {
-        snowOff.querySelector("svg").setAttribute("fill", "#3a5c88");
-      }
+      snowOn.querySelector("svg").setAttribute("fill", "#444");
+      snowOff.querySelector("svg").setAttribute("fill", "#3a5c88");
       break;
   }
 }
